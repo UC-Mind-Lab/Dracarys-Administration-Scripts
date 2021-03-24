b5e10f9060a001adc47348a57da9b1a3d71dd364
Wayne Stegner <51062260+stegnerw@users.noreply.github.com>
2021-03-23 20:50:28 -0400

    Remove Android/KVM things
    If Dracarys needs to be reinstalled, we will not include Android emulation software.

--- usr/local/sbin/addUser.sh
+++ usr/local/sbin/addUser.sh
@@ -17,16 +17,11 @@ sudo adduser $username
 sudo passwd -e -l $username
 
 # Groups they should be added to
-sudo adduser $username libvirt # For KVM acceleration for AndroidStudio
-sudo adduser $username kvm # For access to KVM for AndroidStudio
 sudo adduser $username MindLabMember # For access to the public folder
 
 userhome=/home/$username
 # Add a symbolic link to the public folder
 sudo -u $username ln -s /home/MindLabMember $userhome/Public
-# Set up link to Android SDK
-sudo -u $username mkdir -p $userhome/Android
-sudo -u $username ln -s /home/MindLabMember/Android/Sdk $userhome/Android/
 # Set up Xilinx
 sudo -u $username mkdir -p $userhome/.Xilinx
 sudo -u $username ln -s /opt/xilinx/Xilinx.lic $userhome/.Xilinx/
